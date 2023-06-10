56fb46ba6fd4ab1066aaa3f917fd75402cece6ac
qanh2204 <qanh2204@gmail.com>
2023-06-10 18:06:00 +0700

    upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CP2296F02_Group4_InstituteOfFineArts.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
